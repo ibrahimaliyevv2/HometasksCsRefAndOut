c368216cddfa19b85f2bc64ad597e2c50ac4b8c4
ibrahimaliyevv2 <7cauc3b@code.edu.az>
2022-03-18 10:25:06 +0400

    hometasks added

---
+++ HometasksCsRefAndOut/Program.cs
@@ -0,0 +1,94 @@
+﻿using System;
+
+namespace HometasksCsRefAndOut
+{
+    class Program
+    {
+
+        
+        static void Main(string[] args)
+        {
+            #region Hometask-1 Addition
+            //int[] numbers = { 12, 13, 14, 15 };
+            //int num = 16;
+            //AddNumber(ref numbers, num);
+            #endregion
+
+            #region Hometask-2 Addition
+            //string str = "Elimerdan bey Topcubasov";
+            //string print = RemoveSpaces(ref str);
+            //Console.WriteLine(print);
+            #endregion
+
+            #region Hometask-3 Addition
+            int num = 35;
+            Console.WriteLine(NearestSquareRoot(ref num));
+            #endregion
+        }
+
+
+        //Hometask-1: Parameter olaraq integer array ve integer qebul eden
+        //ve hemin integer value-nu integer array-e yeni element kimi daxil eden metod
+
+        static void AddNumber(ref int[] numbers, int num)
+        {
+            int[] newNumbers = new int[numbers.Length + 1];
+            newNumbers[numbers.Length] = num;
+            //Burada size olaraq [newNumbers.Length - 1] de ola biler,
+            //cunki numbers.Length ile beraberdir.
+            for (int i = 0; i < numbers.Length; i++)
+            {
+                newNumbers[i] = numbers[i];
+            }
+            numbers = newNumbers;
+
+            foreach (var item in numbers)
+            {
+                Console.WriteLine(item);
+            }
+        }
+
+
+        //Hometask-2: Parameter olaraq bir string deyer qebul edir ve ondaki bosluqlari silir
+        //Meselen, "Hikmet Abbasov" --> "HikmetAbbasov"
+
+        static string RemoveSpaces(ref string text)
+        {
+            string newText = "";
+            for (int i = 0; i < text.Length; i++)
+            {
+                if (text[i] != ' ')
+                {
+                    newText += text[i];
+                }
+            }
+            text = newText;
+            return text;
+        }
+
+
+        //Hometask-3: Qebul etdiyi integer hemin parametrin deyerinin en yaxin kokalti
+        //deyerini menimseden metod
+        //Meselen int num=35 versek, bu variable-in yeni deyeri 5 olmalidir/
+
+        static int NearestSquareRoot(ref int num)
+        {
+            int answer = 0;
+            int i = 2;
+            if(num == 0 || num == 1)
+            {
+                return num;
+            }
+            else if (num > 1)
+            {
+                while(i*i <= num)
+                {
+                    answer = i;
+                    i++;
+                }
+                num = answer;
+            }
+            return num;
+        }
+    }
+}
